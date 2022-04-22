846e1015f6710ff72656fafeb327bb040a9235c4
Santiago Severo <santiagosevero28@gmail.com>
2022-04-21 21:46:35 -0300

    GameOfLife

---
+++ src/Library/Tablero.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Gameoflife
+{
+    public static class Tablero
+    {
+        public static void Logica()
+        {
+            bool[,] gameBoard = Inicializador.Inicio() /* contenido del tablero */;
+            int boardWidth = gameBoard.GetLength(0);
+            int boardHeight = gameBoard.GetLength(1);
+            bool[,] cloneboard = new bool[boardWidth, boardHeight];
+            while (true)
+            {
+                for (int x = 0; x < boardWidth; x++)
+                {
+                    for (int y = 0; y < boardHeight; y++)
+                    {
+                        int aliveNeighbors = 0;
+                        for (int i = x - 1; i <= x + 1; i++)
+                        {
+                            for (int j = y - 1; j <= y + 1; j++)
+                            {
+                                if (i >= 0 && i < boardWidth && j >= 0 && j < boardHeight && gameBoard[i, j])
+                                {
+                                    aliveNeighbors++;
+                                }
+                            }
+                        }
+                        if (gameBoard[x, y])
+                        {
+                            aliveNeighbors--;
+                        }
+                        if (gameBoard[x, y] && aliveNeighbors < 2)
+                        {
+                            //Celula muere por baja población
+                            cloneboard[x, y] = false;
+                        }
+                        else if (gameBoard[x, y] && aliveNeighbors > 3)
+                        {
+                            //Celula muere por sobrepoblación
+                            cloneboard[x, y] = false;
+                        }
+                        else if (!gameBoard[x, y] && aliveNeighbors == 3)
+                        {
+                            //Celula nace por reproducción
+                            cloneboard[x, y] = true;
+                        }
+                        else
+                        {
+                            //Celula mantiene el estado que tenía
+                            cloneboard[x, y] = gameBoard[x, y];
+                        }
+                    }
+                }
+                gameBoard = cloneboard;
+                ImprimirTablero.Imprimir(gameBoard);
+                cloneboard = new bool[boardWidth, boardHeight];
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace PII_Game_Of_Life
+namespace Gameoflife
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Tablero.Logica();
         }
     }
 }
